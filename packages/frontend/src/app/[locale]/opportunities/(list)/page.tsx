629c681120d1fc02e10dfb6e95adfd444eb83781
Paolo Guerra <pg.guerrap@proton.me>
2026-08-10 14:42:55 +0200

    feat(frontend): improve opportunities page metadata title

--- packages/frontend/src/app/[locale]/opportunities/(list)/page.tsx
+++ packages/frontend/src/app/[locale]/opportunities/(list)/page.tsx
@@ -15,7 +15,7 @@ export async function generateMetadata({
     const t = await getTranslations("opportunities");
 
     return {
-        title: t("title"),
+        title: t("metaTitle"),
         description: t("subtitle"),
         alternates: { canonical: `/${locale}/opportunities` },
     };
